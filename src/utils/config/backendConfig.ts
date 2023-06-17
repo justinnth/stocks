0da1766ce3bc0b95ed322215098f0a5daaf6dee4
Justin North <justin_n@hotmail.fr>
2023-06-17 12:53:07 +0200

    :sparkles: Typed env variables and set env variables for Supertokens

--- src/utils/config/backendConfig.ts
+++ src/utils/config/backendConfig.ts
@@ -9,29 +9,27 @@ export const backendConfig = (): TypeInput => {
   return {
     framework: "express",
     supertokens: {
-      connectionURI:
-        "https://dev-d3a72c810c8b11eea5b579bfe02c0a22-eu-west-1.aws.supertokens.io:3572",
-      apiKey: "euJxXReryVTAbM1eFLpR8NfI0m-HuP",
+      connectionURI: process.env.SUPERTOKENS_CONNECTION_URI,
+      apiKey: process.env.SUPERTOKENS_API_KEY,
     },
     appInfo,
     recipeList: [
       ThirdPartyEmailPasswordNode.init({
         providers: [
           ThirdPartyEmailPasswordNode.Google({
-            clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-            clientSecret: "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",
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           }),
           ThirdPartyEmailPasswordNode.Github({
-            clientId: "467101b197249757c71f",
-            clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
           }),
           ThirdPartyEmailPasswordNode.Apple({
-            clientId: "4398792-io.supertokens.example.service",
+            clientId: process.env.APPLE_CLIENT_ID,
             clientSecret: {
-              keyId: "7M48Y4RYDL",
-              privateKey:
-                "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-              teamId: "YWQCXGJRJL",
+              keyId: process.env.APPLE_KEY_ID,
+              privateKey: process.env.APPLE_PRIVATE_KEY,
+              teamId: process.env.APPLE_TEAM_ID,
             },
           }),
         ],
---
+++ src/utils/env.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+
+const envVariables = z.object({
+  SUPERTOKENS_CONNECTION_URI: z.string(),
+  SUPERTOKENS_API_KEY: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  APPLE_CLIENT_ID: z.string(),
+  APPLE_KEY_ID: z.string(),
+  APPLE_PRIVATE_KEY: z.string(),
+  APPLE_TEAM_ID: z.string(),
+})
+
+envVariables.parse(process.env)
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
